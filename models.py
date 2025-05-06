588b0bc685bdd8d11ff3af0868fc1910d056c655
dezhdzo <denione@gmail.com>
2025-05-06 16:52:59 +0200

    base client

--- models.py
+++ models.py
@@ -1,13 +1,18 @@
-from sqlalchemy import Column, Integer, String, Float, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, DateTime
 from database import Base
-from datetime import datetime
+from datetime import datetime, timezone
 
-class ItemDB(Base):
-    __tablename__ = "Products"
-    
-    name = Column(String, index=True)
-    price = Column(Float)
-    description = Column(String)
-    color = Column(String)
-    stock = Column(Integer)
-    id = Column(Integer, primary_key=True, index=True)
+class Itemdb(Base):
+    __tablename__ = "clients"
+
+    id = Column(Integer, primary_key=True)  
+    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    name = Column(String)
+    username = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    address_postal_code = Column(String)
+    address_city = Column(String)
+    profile_first_name = Column(String)
+    profile_last_name = Column(String)
+    company_name = Column(String)
